a3798e7eae54fa094deaedd6ad92c449265e5f76
muflon182 <mateuszmazur182@gmail.com>
2021-06-08 02:04:56 +0200

    when in productadd.php brand select allows to access information from database

--- admin/productadd.php
+++ admin/productadd.php
@@ -1,93 +1,123 @@
-﻿<?php include 'inc/header.php';?>
-<?php include 'inc/sidebar.php';?>
+﻿<?php include 'inc/header.php'; ?>
+<?php include 'inc/sidebar.php'; ?>
+<?php include '../classes/Category.php'; ?>
+<?php include '../classes/Brand.php';  ?>
+
 <div class="grid_10">
     <div class="box round first grid">
         <h2>Add New Product</h2>
-        <div class="block">               
-         <form action="" method="post" enctype="multipart/form-data">
-            <table class="form">
-               
-                <tr>
-                    <td>
-                        <label>Name</label>
-                    </td>
-                    <td>
-                        <input type="text" placeholder="Enter Product Name..." class="medium" />
-                    </td>
-                </tr>
-				<tr>
-                    <td>
-                        <label>Category</label>
-                    </td>
-                    <td>
-                        <select id="select" name="select">
-                            <option>Select Category</option>
-                            <option value="1">Category One</option>
-                            <option value="2">Category Two</option>
-                            <option value="3">Category Three</option>
-                        </select>
-                    </td>
-                </tr>
-				<tr>
-                    <td>
-                        <label>Brand</label>
-                    </td>
-                    <td>
-                        <select id="select" name="select">
-                            <option>Select Brand</option>
-                            <option value="1">Brand One</option>
-                            <option value="2">Brand Two</option>
-                            <option value="3">Brand Three</option>
-                        </select>
-                    </td>
-                </tr>
-				
-				 <tr>
-                    <td style="vertical-align: top; padding-top: 9px;">
-                        <label>Description</label>
-                    </td>
-                    <td>
-                        <textarea class="tinymce"></textarea>
-                    </td>
-                </tr>
-				<tr>
-                    <td>
-                        <label>Price</label>
-                    </td>
-                    <td>
-                        <input type="text" placeholder="Enter Price..." class="medium" />
-                    </td>
-                </tr>
-            
-                <tr>
-                    <td>
-                        <label>Upload Image</label>
-                    </td>
-                    <td>
-                        <input type="file" />
-                    </td>
-                </tr>
-				
-				<tr>
-                    <td>
-                        <label>Product Type</label>
-                    </td>
-                    <td>
-                        <select id="select" name="select">
-                            <option>Select Type</option>
-                            <option value="1">Featured</option>
-                            <option value="2">Non-Featured</option>
-                        </select>
-                    </td>
-                </tr>
+        <div class="block">
+            <form action="" method="post" enctype="multipart/form-data">
+                <table class="form">
 
-				<tr>
-                    <td></td>
-                    <td>
-                        <input type="submit" name="submit" Value="Save" />
-                    </td>
-                </tr>
-            </table>
+                    <tr>
+                        <td>
+                            <label>Name</label>
+                        </td>
+                        <td>
+                            <input type="text" name="productName" placeholder="Enter Product Name..." class="medium" />
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>
+                            <label>Category</label>
+                        </td>
+                        <td>
+                            <select id="select" name="catId">
+                                <option>Select Category</option>
+                                <?php
+                                $cat = new Category(); //creating object for this class
+                                $getCat = $cat->getAllCat();  //get category and access method getAllCat to read data
+                                if ($getCat) {
+                                    while ($result = $getCat->fetch_assoc()) {
+
+
+
+
+                                ?>
+
+                                        <option value=" <?php echo $result['catId']; ?>"> <?php echo $result['catName'];   ?></option> 
+                                <?php }  //this will display Categories and names
+                                } ?> 
+                                <option value="2">Category Two</option>
+                                <option value="3">Category Three</option>
+                            </select>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>
+                            <label>Brand</label>
+                        </td>
+                        <td>
+                            <select id="select" name="brandId">
+                                <option>Select Brand</option>
+                                <?php
+                                $brand = new Brand(); // what happenes here is analogically same as in line 29
+                                
+                                $getBrand = $brand->getAllBrand();  //
+                                if ($getBrand) {
+                                    while ($result = $getBrand->fetch_assoc()) {
+
+
+
+
+                                ?>
+
+                                <option value="<?php echo $result['brandId']   ?>"><?php echo $result['brandName'];  ?></option>
+                                <?php }  
+                                } ?> 
+                                <option value="2">Brand Two</option>
+                                <option value="3">Brand Three</option>
+                            </select>
+                        </td>
+                    </tr>
+
+                    <tr>
+                        <td style="vertical-align: top; padding-top: 9px;">
+                            <label>Description</label>
+                        </td>
+                        <td>
+                            <textarea class="tinymce" name="body"></textarea>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>
+                            <label>Price</label>
+                        </td>
+                        <td>
+                            <input type="text" name="price" placeholder="Enter Price..." class="medium" />
+                        </td>
+                    </tr>
+
+                    <tr>
+                        <td>
+                            <label>Upload Image</label>
+                        </td>
+                        <td>
+                            <input type="file" name="image" />
+                        </td>
+                    </tr>
+
+                    <tr>
+                        <td>
+                            <label>Product Type</label>
+                        </td>
+                        <td>
+                            <select id="select" name="type">
+                                <option>Select Type</option>
+                                <option value="0">Featured</option>
+                                <option value="1">General</option>
+                            </select>
+                        </td>
+                    </tr>
+
+                    <tr>
+                        <td></td>
+                        <td>
+                            <input type="submit" name="submit" Value="Save" />
+                        </td>
+                    </tr>
+                </table>
             </form>
         </div>
     </div>
@@ -95,7 +125,7 @@
 <!-- Load TinyMCE -->
 <script src="js/tiny-mce/jquery.tinymce.js" type="text/javascript"></script>
 <script type="text/javascript">
-    $(document).ready(function () {
+    $(document).ready(function() {
         setupTinyMCE();
         setDatePicker('date-picker');
         $('input[type="checkbox"]').fancybutton();
@@ -103,6 +133,4 @@
     });
 </script>
 <!-- Load TinyMCE -->
-<?php include 'inc/footer.php';?>
-
-
+<?php include 'inc/footer.php'; ?>
