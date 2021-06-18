3a8e875824fc8842c2d252f38a0b7038bc37db58
tomaszgaj <89tomaszgaj@gmail.com>
2021-06-19 00:51:35 +0200

    Update-Delete-Load-Feature,  Added Cart Class

--- classes/Product.php
+++ classes/Product.php
@@ -1,6 +1,9 @@
 <?php
-include_once '../lib/Database.php';
-include_once '../helpers/Format.php';
+$filepath = realpath(dirname(__FILE__));
+include_once ($filepath.'/../lib/Database.php');
+include_once ($filepath.'/../helpers/Format.php');
+
+
 
 ?>
  
@@ -35,7 +38,7 @@ class Product
         $div = explode('.', $file_name);
         $file_ext = strtolower(end($div));
         $unique_image = substr(md5(time()), 0, 10) . '.' . $file_ext;
-        $uploaded_image = "upload/".$unique_image;
+        $uploaded_image = "upload/" . $unique_image;
         if ($productName == "" || $catId == "" || $brandId == "" || $body == "" || $price == "" || $type == "") {
             $msg = "<span class='error'>Field Must Not be empty .</span> ";
             return $msg;
@@ -54,4 +57,149 @@ class Product
             }
         }
     }
+    public function getAllProduct()
+    {
+        $query = "SELECT tbl_product.*, tbl_category.catName, tbl_brand.brandName 
+        FROM  tbl_product 
+        INNER JOIN tbl_category
+        ON tbl_product.catId = tbl_category.catId
+        INNER JOIN tbl_brand
+        ON tbl_product.brandId = tbl_brand.brandId
+        ORDER BY tbl_product.productId DESC
+        "; //selecting matching value so we can display the actual product Category name instead of Id
+
+
+        $result = $this->db->select($query);
+        return $result;
+    }
+    public function getProById($id)
+    {
+        $query = "SELECT * FROM tbl_product WHERE productId ='$id' ";
+        $result = $this->db->select($query);
+        return $result;
+    }
+    public function productUpdate($data, $file, $id)
+    {
+
+        $productName     =  mysqli_real_escape_string($this->db->link, $data['productName']);
+        $catId          =  mysqli_real_escape_string($this->db->link, $data['catId']);
+        $brandId          =  mysqli_real_escape_string($this->db->link, $data['brandId']);
+        $body          =  mysqli_real_escape_string($this->db->link, $data['body']);
+        $price           =  mysqli_real_escape_string($this->db->link, $data['price']);
+        $type          =  mysqli_real_escape_string($this->db->link, $data['type']);
+
+        $permited = array('jpg', 'png', 'jpeg', 'gif');
+        $file_name = $file['image']['name'];
+        $file_size = $file['image']['size'];
+        $file_temp = $file['image']['tmp_name'];
+
+        $div = explode('.', $file_name);
+        $file_ext = strtolower(end($div));
+        $unique_image = substr(md5(time()), 0, 10) . '.' . $file_ext;
+        $uploaded_image = "upload/" . $unique_image;
+        if ($productName == "" || $catId == "" || $brandId == "" || $body == "" || $price == "" || $type == "") {
+            $msg = "<span class='error'>Field Must Not be empty .</span> ";
+            return $msg;
+        } else {
+            if (!empty($file_name)) {
+                if ($file_size > 1054589) {
+                    echo "<span class='error'>Image Size should be less then 1MB .</span>";
+                } elseif (in_array($file_ext, $permited) === false) {
+                    echo "<span class='error'> You can Upload Only" . implode(',', $permited) . "</span>";
+                } else {
+                    move_uploaded_file($file_temp, $uploaded_image);
+                    $query = "UPDATE tbl_product
+          SET 
+          productName 	= '$productName',
+          catId 		= '$catId',
+          brandId 		= '$brandId',
+          body 			= '$body',
+          price 		= '$price',
+          image 		= '$uploaded_image',
+          type 			= '$type'
+          WHERE productId = '$id' ";
+
+                    $updated_row = $this->db->update($query);
+                    if ($updated_row) {
+                        $msg = "<span class='success'>Product Updated Successfully.</span> ";
+                        return $msg;
+                    } else {
+                        $msg = "<span class='error'>Product Not Updated .</span> ";
+                        return $msg;
+                    }
+                }
+            } else {
+                $query = "UPDATE tbl_product
+          SET 
+          productName 	= '$productName',
+          catId 		= '$catId',
+          brandId 		= '$brandId',
+          body 			= '$body',
+          price 		= '$price',
+          
+          type 			= '$type'
+          WHERE productId = '$id' ";
+
+                $updated_row = $this->db->update($query);
+                if ($updated_row) {
+                    $msg = "<span class='success'>Product Updated Successfully.</span> ";
+                    return $msg; // return This Message 
+                } else {
+                    $msg = "<span class='error'>Product Not Updated .</span> ";
+                    return $msg; // return This Message 
+                }
+            }//$dellink = &delImg['image'];
+        }
+    }
+    public function delProById($id){
+        $query = "SELECT * FROM tbl_product WHERE productId = '$id' ";
+        $getData = $this->db->select($query);
+        if ($getData){
+            while ($delImg = $getData->fetch_assoc()){
+                $dellink = $delImg['image'];
+                unlink($dellink);
+            }
+        }
+        $delquery = "DELETE FROM tbl_product WHERE productId = '$id' ";
+        $deldata = $this->db->delete($delquery);
+        if ($deldata) {
+            $msg = "<span class='success'>Product Deleted Successfully.</span> ";
+            return $msg; // return this Message 
+        } else {
+            $msg = "<span class='error'>Product Not Deleted .</span> ";
+            return $msg; // return this Message 
+        }
+    }
+
+    public function getFeaturedProduct() {
+        $query = "SELECT * FROM tbl_product WHERE type='0' ORDER BY productId DESC LIMIT 6 ";
+      $result = $this->db->select($query);
+      return $result;
+   }
+   public function getNewProduct(){
+    $query = "SELECT * FROM tbl_product ORDER BY productId DESC LIMIT 4 ";
+    $result = $this->db->select($query);
+    return $result;
+
+   }
+   public function getSingleProduct($id){
+    $query = "SELECT tbl_product.*, tbl_category.catName, tbl_brand.brandName
+    FROM tbl_product
+    INNER JOIN tbl_category
+    ON tbl_product.catId = tbl_category.catId
+    INNER JOIN tbl_brand
+    ON tbl_product.brandId = tbl_brand.brandId
+    AND tbl_product.productId = $id
+    ORDER BY tbl_product.productId DESC";
+$result =  $this->db->select($query);
+return $result;  // i return this result 
+
+   }
+
+
+
 }
+
+
+
+?>
